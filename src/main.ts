386b67bcfed3b2bebf339d9e072c082a09045325
Kevin L <kevin.lecouvey@gmail.com>
2020-12-09 21:42:16 -0500

    Fix semver-compare import

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import * as installer from './installer'
 async function run(): Promise<void> {
   try {
     const version: string = core.getInput('version')
-    const checkLatest: boolean = (core.getInput('checkLatest').toLowerCase() == 'true');
-
-    
+    const checkLatest: boolean =
+      core.getInput('checkLatest').toLowerCase() == 'true'
   } catch (error) {
     core.setFailed(error.message)
   }
